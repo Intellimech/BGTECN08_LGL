2a8c6f738bf7c91c9311cded7e60e5f18f2eb10b
RohanVangal <vangalrohan@gmail.com>
2023-05-09 16:40:50 +0200

    Updated table insert format

--- SqlManager.py
+++ SqlManager.py
@@ -6,15 +6,17 @@ from sqlalchemy.dialects.mysql.dml import Insert
 import datetime as dt
 
 class SqlManager:
-    acq_dflt = {
-                "filename":'Error',
-                "timestamp":'01-01-2000',
-                "conn_status": 'test',
-                "machine_status": 0,
-                "inv_speed": 0,
-                "machine_power": 1,
-                "RoundCount": 0
+    log_dflt = {
+                "MachineID":'Error',
+                "FeederID": '0',
+                "Timestamp": 0,
+                "AlertName": 'yarn-break',
+                "AlertForecastMins": 1440
                 }
+    realtime_dflt = {
+                     "MachineID":'Error',
+                     "FeederID": '0'
+                    }
 
     dat_fields = {"Status":"Status",
               "T.read dgr":"T_read",
@@ -102,94 +104,100 @@ class SqlManager:
                     except Exception as e:
                         logging.error(f"ERROR inserting data to 'Dati':\n{str(e)}")
 
-    def insert_acq(self, ip_json):
+    def insertToTables(self, ip_json):
         """
-        Inserts json data in the Acquisizioni table
-        
-        :param ip_json: Input JSON message
-        :param engine: SQL engine
-        :return: Returns id_acq of the inserted line for use in updating data table.
-                 Returns none if json does not contain the necessary fields
+        Inserts json data into the tables LogPrediction Data and RtpredictionData
+        @param ip_json: Input JSON message to extract data from
         """
         db_metadata = MetaData(bind = self.engine)
         db_metadata.reflect()
 
-        temp = ip_json["GeneralInfo"]
-        topic = ip_json["Topic"]
         #Copy default values
-        dic_values = self.acq_dflt
-        #Fill values from JSON
-        # for key in temp.keys():
-        #     if acq_fields[key] in dic_values.keys():
-        #         if key == "Customer" and "Time" in temp.keys():
-        #             dic_values[acq_fields[key]] = temp[key]+temp["Time"]
-        #         else:
-        #             dic_values[acq_fields[key]] = temp[key]
+        log_dic_values = self.log_dflt
+        rt_dic_values = self.realtime_dflt
+
+        #Get primary key values from JSON
+        timestamp = dt.datetime.fromtimestamp(ip_json["timestamp"]/1000.0)    #Convert from epoch time in ms to datetime
+        log_dic_values["Timestamp"] = timestamp
+        rt_dic_values["Timestamp"] = timestamp
+
+        for elem in ip_json["metadatas"]:
+            # Add MachineID to value dictionary
+            if elem["name"] == "machine-id":
+                log_dic_values["MachineID"] = elem["text"]
+                rt_dic_values["MachineID"] = elem["text"]
+            # Add FeederID to value dictionary
+            if elem["name"] == "feeder-id":
+                log_dic_values["FeederID"] = int(elem["text"])
+                rt_dic_values["FeederID"] = int(elem["text"])
         
-        # Check primary keys in JSON message
-        acq_primary_keys = ["Customer","Time","MachineDescriptor"]
-        ifresult = True
-        if not all(pkey in temp.keys() for pkey in acq_primary_keys):
-            logging.info("Message recieved did not contain the necessary fields")
-            return (None)
-
-        # Add data to a dictionary to be inserted
-        dic_values["filename"] = str(temp["Customer"]+temp["Time"])
-        dic_values["timestamp"] = dt.datetime.strptime(temp["Time"], '%d_%m_%Y-%H_%M_%S')    #.strftime('%Y-%m-%d')
-        dic_values["conn_status"] = str(temp["Connection_Status"]) if temp["Connection_Status"] != '' else dic_values["conn_status"]
-        dic_values["machine_status"] = int(temp["MachineStatus"]) if temp["MachineStatus"] != '' else dic_values["machine_status"]
-        dic_values["inv_speed"] = int(temp["InverterSpeed"]) if temp["InverterSpeed"] != '' else dic_values["inv_speed"]
-        dic_values["RoundCount"] = int(temp["RoundCnt"]) if temp["RoundCnt"] != '' else dic_values["RoundCount"]
-        dic_values["MachineDescriptor"] = str(temp["MachineDescriptor"])
-        dic_values["MachineID"] = topic
-
-        # print(dic_values)
+        #Insert data into RtPredictionData
+        #TODO: Establish alarm codes
+        rt_dic_values["MachineStatus"] = 'Alarm'
         with self.engine.connect() as connection:
             with connection.begin():
-                table = db_metadata.tables[self.t_acq] 
-                #BUG with ON_DUPLICATE_UPDATE that autoincrements even when duplicate is found  
-                #SOLUTION check an entry for the same filename as you are about to enter
-                #         if it exists update the table, else it inserts         
-                stmt_sel =  table.select().\
-                            where(and_(
-                                table.c.filename == dic_values["filename"],
-                                table.c.MachineDescriptor == dic_values["MachineDescriptor"]))
+                table = db_metadata.tables["RtPredictionData"]            
+                stmt_ins = (Insert(table).
+                    values(rt_dic_values))
+                stmt_dup = stmt_ins.on_duplicate_key_update(rt_dic_values)
                 try:
-                    res_sel = connection.execute(stmt_sel)
+                    connection.execute(stmt_dup)
                 except Exception as e:
-                    logging.error(f"SQL ERROR in insert_acq():\n{str(e)}")
-                    self.run_flag = False
-                    return(None)
+                    logging.error(f"ERROR inserting data to 'LogPredictionData':\n{str(e)}")
 
-                if res_sel.rowcount > 0:
-                    stmt = table.update().\
-                            where(and_(
-                                table.c.filename == dic_values["filename"],
-                                table.c.MachineDescriptor == dic_values["MachineDescriptor"]
-                                )).values(dic_values)
-                    try:
-                        result = connection.execute(stmt)
-                    except Exception as e:
-                        logging.error(f"SQL ERROR in insert_acq():\n{str(e)}")
-                        self.run_flag = False
-                        return(None)
-                    id = res_sel.all()[0][0]
-                else:
-                    stmt = (Insert(table).values(dic_values))
-                    try:
-                        result = connection.execute(stmt)
-                    except Exception as e:
-                        logging.error(f"SQL ERROR in insert_acq():\n{str(e)}")
-                        self.run_flag = False
-                        return(None)
-                    #Get autoincremented
-                    id = result.inserted_primary_key[0]
-            return(id)
-        return(None)
+        #Get diagnostics
+        for elem in ip_json["diagnostics"]:
+            # Add MachineID to value dictionary
+            if elem["name"] == "alert-category":
+                log_dic_values["AlertCategory"] = elem["value"]
+            # Add MachineID to value dictionary
+            if elem["name"] == "alert-forecast-minutes":
+                log_dic_values["AlertForecastMins"] = int(elem["value"])
+
+        #Insert data into LogPredictionData
+        with self.engine.connect() as connection:
+            with connection.begin():
+                table = db_metadata.tables["LogPredictionData"]            
+                stmt_ins = (Insert(table).
+                    values(log_dic_values))
+                stmt_dup = stmt_ins.on_duplicate_key_update(log_dic_values)
+                try:
+                    connection.execute(stmt_dup)
+                except Exception as e:
+                    logging.error(f"ERROR inserting data to 'LogPredictionData':\n{str(e)}")
     
     def insertData(self):
         while len(self.insertQ) > 0:
             inJSON = self.insertQ.pop()
-            id_acq = self.insert_acq(inJSON)
-            if(id_acq):
-                self.insert_dat(inJSON,id_acq)
+            self.insertToTables(inJSON)
+
+import json
+if __name__ == "__main__":
+    sm = SqlManager()
+    str_json = """{
+    "gatewayId":"gtw-lgl-alarm",
+    "deviceId":"Macchina08-53",
+    "timestamp":1682698026401,
+    "diagnostics":[
+        {
+            "name":"alert-forecast-minutes",
+            "value":"1440"
+        },
+        {
+            "name":"alert-category",
+            "value":"yarn-break"
+        }
+    ],
+    "metadatas":[
+        {
+            "name":"machine-id",
+            "text":"Macchina08"
+        },
+        {
+            "name":"feeder-id",
+            "text":"53"
+        }
+    ]
+    }"""
+    py_json = json.loads(str_json)
+    sm.insertToTables(py_json)
